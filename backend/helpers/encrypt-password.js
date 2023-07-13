4fad80b185a877cf1a0fa6f2583fcb094d4a00a2
Matheus <maatheus.santos223@gmail.com>
2023-07-13 07:20:32 -0300

    Função auxiliar que faz a encriptação da senha

---
+++ backend/helpers/encrypt-password.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcryptjs');
+
+const encryptingPass = async (pass) => {
+
+    const password = pass.toString();
+
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    return passwordHash;
+
+}
+
+module.exports = encryptingPass;
