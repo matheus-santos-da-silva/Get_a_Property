28f43aa45010a46a22e35a507512c0e44062e4b0
Matheus <maatheus.santos223@gmail.com>
2023-07-24 17:38:49 -0300

    Função auxiliar que cuida do upload de fotos com a lib multer

--- backend/helpers/image-upload.js
+++ backend/helpers/image-upload.js
@@ -5,7 +5,7 @@ const imageStorage = multer.diskStorage({
 
     destination: function (req, file, cb) {
 
-        cb(null, 'public/images/propertys');
+        cb(null, 'public/images/properties');
 
     },
     filename: function (req, file, cb) {
