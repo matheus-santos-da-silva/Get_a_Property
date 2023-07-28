59c1f8d65b14512af500df27879d65dfc56857e8
Matheus <maatheus.santos223@gmail.com>
2023-07-28 07:43:38 -0300

    Criados os componentes de layout o formulário de registro

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+
+    return (
+        <footer className={styles.footer}>
+            <p>
+                <span className="bold">Get a Property</span> &copy; 2023
+            </p>
+        </footer>
+    )
+
+}
+
+export default Footer
