450126744790187ed902d1aa3d9ce20715a39cd3
Matheus <maatheus.santos223@gmail.com>
2023-08-01 09:46:37 -0300

    Adicionado o UserProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,24 +5,32 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Container from './components/layout/Container'
+import Message from './components/layout/Message'
 
 /* pages */
 import Login from './components/pages/Auth/Login'
 import Register from './components/pages/Auth/Register'
 import Home from './components/pages/Home'
 
+/* context */
+import { UserProvider } from './context/UserContext'
+
+
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Container>
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Container>
-      <Footer />
+      <UserProvider>
+        <Navbar />
+        <Message />
+        <Container>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </UserProvider>
     </Router>
   );
 }
